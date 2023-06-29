bb7990e568f9d3744d3e77c82e449a68adc4d163
Karen Morais <karen.ksmorais@gmail.com>
2023-06-29 11:51:27 -0300

    cont gems2

---
+++ METODOS E GEMS/gems2.rb
@@ -0,0 +1,17 @@
+# instalar mais de uma gem ao mesmo tempo:
+
+#Bundler
+
+#Passo a ppasso:
+# 1. criar pasta nova 
+# 2. instalar o bundler 
+
+#gem bundler
+
+#dentro da pasta bundler criar arquivo chamado Gemfile e colocar:
+
+#source 'https://rubygems.org'
+
+#gem 'os'
+
+#gem (pegar em um site)

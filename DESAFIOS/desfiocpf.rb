da3b472ccc417aff843534a4b93be4f5697a5fdc
Karen Morais <karen.ksmorais@gmail.com>
2023-06-29 13:02:01 -0300

    atualização correta

--- DESAFIOS/desfiocpf.rb
+++ DESAFIOS/desfiocpf.rb
@@ -1,17 +1,17 @@
 require 'cpf_cnpj'
 
 def cpf (cpf_number)
-    if CPF.valid?
+    if CPF.valid? (cpf_number)
         'CPF VÁLIDO'
     else 
-        'CPF INVÁLIDO'
+        'Não identificamos o CPF'
     end 
 end
 
-print "DIGITE SEU CPF"
+
+print "DIGITE SEU CPF: "
 cpf_number = gets.chomp.to_i
 
-result = cpf_number
+result = cpf (cpf_number)
 
 puts result
-

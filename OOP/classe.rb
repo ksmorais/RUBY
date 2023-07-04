3c3d7a69fdcbeec3f516e8e16f9bcab3bbfbd87b
Karen Morais <karen.ksmorais@gmail.com>
2023-07-04 14:15:57 -0300

    conteudo

---
+++ OOP/classe.rb
@@ -0,0 +1,7 @@
+#definindo classe:
+#A classee é uma base para o objeto
+
+#ex: Contruir um casa (objeto), para isso é necessário um projeto. 
+#A classe é a descrição de como será esse objeto
+# socinha é apenas um deseho, mas junto ao objeto começa se algo prático
+#propriedades desse objeto (metodos): tamnho, quantos quartos...

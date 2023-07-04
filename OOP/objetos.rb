652e3eed01be9409e7416311ed584b96405ca24e
Karen Morais <karen.ksmorais@gmail.com>
2023-07-04 14:15:44 -0300

    novos arquivos

---
+++ OOP/objetos.rb
@@ -0,0 +1,5 @@
+#definição: o que for material e puder ser percebida, objeto do mundo real
+#todos os objetos possuem caracteristicas em comum
+#ex: cachorro
+#ex: informações: sao os atributos: tamanho, raça, cor. peso
+#ex: comportamento: são metodos: latir, comer, morder, dormir. 

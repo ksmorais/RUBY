a1a56cccfee2cabd374cf346c27049cb29fa8fbe
Karen Morais <karen.ksmorais@gmail.com>
2023-07-04 12:48:26 -0300

    Resolução concluida dos desafios

--- DESAFIOS/desafio1basicos2.rb
+++ DESAFIOS/desafio1basicos2.rb
@@ -5,3 +5,20 @@
 # puts "O resultado foi = #{number + 2}"
 
 #Desafio 2/3
+
+# temp = gets.chomp.to_i
+
+# if temp > 25
+#   puts "Está calor!"
+# end
+
+#Desafio 3/3
+
+# number1 = gets.to_i
+# number2 =gets.to_i
+
+# if number1 > number2
+#   puts " O número #{number1} é o maior e o #{number2} é o menor"
+#   elsif number2 > number1
+#     puts "O número #{number2} é o maior e o #{number1} é o menor"
+# end

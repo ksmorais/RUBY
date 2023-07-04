afbb5298cb882d8cf185108a5150640ffe4ffb36
Karen Morais <karen.ksmorais@gmail.com>
2023-07-04 17:41:59 -0300

    Conteudo de herança

---
+++ OOP/herança.rb
@@ -0,0 +1,24 @@
+#herança
+#p herdar caracteristica de outra classe, adicione na frente do nom
+#nome de uma classe filha o simb menor e depois o nome da classe pai
+
+#ex animal
+
+class animal
+    def dorme
+        "ZzZzzZ"
+    def pula 
+        "toin, toin"
+    end
+end
+
+class Gato < animal
+    def miar
+        "miau"
+    end
+end
+
+Gato = Gato.new
+puts gato.miar
+puts gato.dorme
+puts gato.pula

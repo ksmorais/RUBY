03c5e5f548dd0a7c416bfe22e366f8e3f5702005
Karen Morais <karen.ksmorais@gmail.com>
2023-07-05 13:45:24 -0300

    novos conteudos

--- OOP/escopo.rb
+++ OOP/escopo.rb
@@ -1,1 +1,20 @@
 #Define onde uma variavel estara disponivel dentro do seu programa 
+# 4 tipos:
+# 1. Locais
+# 2. Globais
+# 3. Classes
+# 4. Instância
+
+#locais: Podem ser acessadas apenas onde foram criadas
+#forma > primeira letra minusculas ou sublinhadas
+
+
+#Globais:  Acessadas de qlq lugar do programa
+#forma > use o prefixo $ 
+# uso desencorajada
+
+#classe: pode ser acessada de qlq lugar da classe
+#forma > @@
+
+#intancia: parecida com a de classe 
+#forma: @
---
+++ OOP/local.rb
@@ -0,0 +1,10 @@
+class Teste
+    def local
+        local = 'é acessa somente nesse metodo local'
+        print local
+    end
+end
+
+teste = Teste.new
+
+teste.local
